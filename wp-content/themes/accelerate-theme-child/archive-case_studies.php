c30478eb92b1736b2c29d9f3d9c086fec72bf3c1
lsavoie <twosavoie@gmail.com>
2018-06-14 15:29:20 -0500

    archive case study page

---
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying archive of case studies
+ *
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div class="main-content" role="main">
+			<?php while ( have_posts() ) : the_post();
+        $image_1 = get_field("image_1");
+        $size = "full";
+        $services = get_field('services');
+      ?>
+
+
+      <article class="case-study clearfix">
+        <aside class="case-study-sidebar">
+          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          <h5><?php echo $services; ?></h5>
+          <?php the_excerpt(); ?>
+          <p class="view-project"><strong><a href="<?php the_permalink(); ?>">View Project</a></strong></p>
+        </aside>
+        <div class="case-study-images">
+          <a href="<?php the_permalink(); ?>">
+            <?php if($image_1) {
+              echo wp_get_attachment_image( $image_1, $size );
+            } ?>
+          </a>
+        </div>
+      </article>
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- .main-content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
