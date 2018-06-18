31e30a34993d041906df3602b4cee6e002448076
lsavoie <twosavoie@gmail.com>
2018-06-18 11:19:48 -0500

    added about page to child theme

---
+++ wp-content/themes/accelerate-theme-child/about.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying the about page
+ *
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div class="main-content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php the_content(); ?>
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- .main-content -->
+
+
+
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
